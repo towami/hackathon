c62fc3187bada8597946fa8bcc99ab9f22c93c17
Mint1 <thomasdepaepe.contact@gmail.com>
2024-11-26 12:05:04 +0100

    Dropped Node, Added Tree and objectloader local, face now displays, should work on AWS

---
+++ server.py
@@ -0,0 +1,16 @@
+# custom_server.py
+import http.server
+import socketserver
+import mimetypes
+
+PORT = 8006
+
+# Ensure '.js' files are served with 'application/javascript' MIME type
+mimetypes.init()  # Initialize the mimetypes
+mimetypes.add_type('application/javascript', '.js')
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
